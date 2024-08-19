0df7f4cca0c3a5d97676619a04399c240aa7a07f
kyji <mdzakysyam1204@gmail.com>
2024-08-19 16:51:01 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 mongoose.connect(process.env.DB_URL)
     .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+    .catch(err => console.log(err));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -15,17 +15,6 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use('/', require('./routes/index'));
-
-// Handle 404 errors
-app.use((req, res) => {
-    res.status(404).render('404'); 
-});
-
-// Handle other errors
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
